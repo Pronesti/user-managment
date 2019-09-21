3ad7a2fe35150a39fe27fb7235ceb51fbfa2d5fe
Diego Pronesti <34582381+Pronesti@users.noreply.github.com>
2019-09-21 15:24:33 -0300

    Initial commit

---
+++ routes/api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('GET request to the homepage');
+});
+
+router.post('/', function(req, res) {
+  res.send('POST request to the homepage');
+});
+
+module.expors = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const api = require('./routes/api/');
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .catch(err => console.log(err));
+app.use(express.json());
+app.use('/api', api);
+
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+app.listen(port, () => console.log(`Example app listening on port port!`));
